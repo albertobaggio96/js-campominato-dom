fa8dd7e9939c3a11c9316ff91df746d2ccc4c3d9
albertobaggio96 <albertobaggio96@gmail.com>
2022-12-01 13:28:42 +0100

    function for casula bomb position

--- js/script.js
+++ js/script.js
@@ -1,44 +1,60 @@
-function getNewDivElement(){
-  const divElement= document.createElement("div");
-  return divElement;
-}
+// function getNewDivElement(){
+//   const divElement= document.createElement("div");
+//   return divElement;
+// }
 
-function getSquare(squareSize, tot){
-  const mainElement= document.querySelector("main");
+// function getSquare(squareSize, tot){
+//   const mainElement= document.querySelector("main");
 
-  mainElement.innerHTML=""
+//   mainElement.innerHTML=""
   
-  const bigSquare= getNewDivElement();
-  bigSquare.classList.add("big-Square", "m-auto", "d-flex", "flex-wrap", "p-0");
+//   const bigSquare= getNewDivElement();
+//   bigSquare.classList.add("big-Square", "m-auto", "d-flex", "flex-wrap", "p-0");
   
-  mainElement.append(bigSquare);
+//   mainElement.append(bigSquare);
   
-  for(let i = 1; i <= tot; i++){
-    let smallSquare= getNewDivElement();
-    smallSquare.classList.add("small-Square", "d-flex", "justify-content-center", "align-items-center", squareSize);
-    bigSquare.append(smallSquare);
-    smallSquare.append(i);
+//   for(let i = 1; i <= tot; i++){
+//     let smallSquare= getNewDivElement();
+//     smallSquare.classList.add("small-Square", "d-flex", "justify-content-center", "align-items-center", squareSize);
+//     bigSquare.append(smallSquare);
+//     smallSquare.append(i);
 
-    smallSquare.addEventListener("click", function(){
-      smallSquare.classList.toggle("active");
-    })
+//     smallSquare.addEventListener("click", function(){
+//       smallSquare.classList.toggle("active");
+//     })
+//   }
+//   return mainElement
+// }
+
+function getCasualBombPosition(howMany, minNum, maxNum){
+  
+  let arr= [];
+  
+  while(arr.length < howMany){
+    const randomNumber= Math.floor(Math.random() * (maxNum - minNum + 1) + minNum);
+    if(!arr.includes(randomNumber)){
+      arr.push(randomNumber);
+    }
   }
-  return mainElement
+
+  return arr;
 }
 
-const buttonSmall= document.getElementById("small");
-const buttonMedium= document.getElementById("medium");
-const buttonBig= document.getElementById("big");
+console.log(getCasualBombPosition(16, 1, 100));
+
+// const buttonSmall= document.getElementById("small");
+// const buttonMedium= document.getElementById("medium");
+// const buttonBig= document.getElementById("big");
 
 
-buttonSmall.addEventListener("click", function(){
-  getSquare("forty-nine", 49)
-})
+// buttonSmall.addEventListener("click", function(){
+//   getSquare("forty-nine", 49)
+// })
 
-buttonMedium.addEventListener("click", function(){
-  getSquare("eighty-one", 81)
-})
+// buttonMedium.addEventListener("click", function(){
+//   getSquare("eighty-one", 81)
+// })
 
-buttonBig.addEventListener("click", function(){
-  getSquare("hundred", 100)
-})
+// buttonBig.addEventListener("click", function(){
+//   getSquare("hundred", 100)
+// })
