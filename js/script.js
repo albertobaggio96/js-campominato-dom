df5a3d0721b592a15eb9d46aff00f311b121cb20
albertobaggio96 <albertobaggio96@gmail.com>
2022-12-01 14:40:27 +0100

    pisitioned bomb

--- js/script.js
+++ js/script.js
@@ -1,30 +1,7 @@
-// function getNewDivElement(){
-//   const divElement= document.createElement("div");
-//   return divElement;
-// }
-
-// function getSquare(squareSize, tot){
-//   const mainElement= document.querySelector("main");
-
-//   mainElement.innerHTML=""
-  
-//   const bigSquare= getNewDivElement();
-//   bigSquare.classList.add("big-Square", "m-auto", "d-flex", "flex-wrap", "p-0");
-  
-//   mainElement.append(bigSquare);
-  
-//   for(let i = 1; i <= tot; i++){
-//     let smallSquare= getNewDivElement();
-//     smallSquare.classList.add("small-Square", "d-flex", "justify-content-center", "align-items-center", squareSize);
-//     bigSquare.append(smallSquare);
-//     smallSquare.append(i);
-
-//     smallSquare.addEventListener("click", function(){
-//       smallSquare.classList.toggle("active");
-//     })
-//   }
-//   return mainElement
-// }
+function getNewDivElement(){
+  const divElement= document.createElement("div");
+  return divElement;
+}
 
 function getCasualBombPosition(howMany, minNum, maxNum){
   
@@ -40,21 +17,54 @@ function getCasualBombPosition(howMany, minNum, maxNum){
   return arr;
 }
 
-console.log(getCasualBombPosition(16, 1, 100));
+function getSquare(squareSize, tot, bomb){
+  const mainElement= document.querySelector("main");
 
-// const buttonSmall= document.getElementById("small");
-// const buttonMedium= document.getElementById("medium");
-// const buttonBig= document.getElementById("big");
+  mainElement.innerHTML=""
+  
+  const bigSquare= getNewDivElement();
+  bigSquare.classList.add("big-Square", "m-auto", "d-flex", "flex-wrap", "p-0");
+  
+  mainElement.append(bigSquare);
+  
+  for(let i = 1; i <= tot; i++){
+    let smallSquare= getNewDivElement();
+    smallSquare.classList.add("small-Square", "d-flex", "justify-content-center", "align-items-center", squareSize);
+    bigSquare.append(smallSquare);
+    smallSquare.append(i);
 
+    if (bomb.includes(i)){
+      smallSquare.addEventListener("click", function(){
+        smallSquare.classList.toggle("bomb");
+      })
+    }else{
+      smallSquare.addEventListener("click", function(){
+        smallSquare.classList.toggle("active");
+      })
+    }
 
-// buttonSmall.addEventListener("click", function(){
-//   getSquare("forty-nine", 49)
-// })
+  }
+  return mainElement
+}
 
-// buttonMedium.addEventListener("click", function(){
-//   getSquare("eighty-one", 81)
-// })
+const buttonSmall= document.getElementById("small");
+const buttonMedium= document.getElementById("medium");
+const buttonBig= document.getElementById("big");
 
-// buttonBig.addEventListener("click", function(){
-//   getSquare("hundred", 100)
-// })
+buttonSmall.addEventListener("click", function(){
+  let randomBomb= getCasualBombPosition(16, 1, 49);
+  console.log(randomBomb);
+  getSquare("forty-nine", 49, randomBomb)
+})
+
+buttonMedium.addEventListener("click", function(){
+  let randomBomb= getCasualBombPosition(16, 1, 81);
+  console.log(randomBomb);
+  getSquare("eighty-one", 81, randomBomb)
+})
+
+buttonBig.addEventListener("click", function(){
+  let randomBomb= getCasualBombPosition(16, 1, 100);
+  console.log(randomBomb);
+  getSquare("hundred", 100, randomBomb);
+})
